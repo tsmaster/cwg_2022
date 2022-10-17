35ce66eca08c60463a21e90e707d3aa4014a45ca
Dave LeCompte <tsmaster@gmail.com>
2022-10-16 17:37:04 -0700

    Initial checkin

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use gilrs::{Gilrs, Button, Event};
+
+
+fn main() {
+    println!("Hello, Cars With(?) Guns 2022!");
+
+
+    let mut gilrs = Gilrs::new().unwrap();
+
+    // Iterate over all connected gamepads
+    for (_id, gamepad) in gilrs.gamepads() {
+	println!("{} is {:?}", gamepad.name(), gamepad.power_info());
+    }
+
+    let mut active_gamepad = None;
+
+    loop {
+	// Examine new events
+	while let Some(Event { id, event, time }) = gilrs.next_event() {
+            println!("{:?} New event from {}: {:?}", time, id, event);
+            active_gamepad = Some(id);
+	}
+
+	// You can also use cached gamepad state
+	if let Some(gamepad) = active_gamepad.map(|id| gilrs.gamepad(id)) {
+            if gamepad.is_pressed(Button::South) {
+		println!("Button South is pressed (XBox - A, PS - X)");
+            }
+	}
+    }
+}
